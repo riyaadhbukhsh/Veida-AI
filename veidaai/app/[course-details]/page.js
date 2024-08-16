73da754a03e26c2f6c7311fac7cc404a918bee7f
Everett Villiger <92830166+boomer199@users.noreply.github.com>
2024-08-16 12:23:56 -0700

    slight bug fix but created a new one

--- veidaai/app/[course-details]/page.js
+++ veidaai/app/[course-details]/page.js
@@ -1,12 +1,11 @@
 "use client";
 
-import { useParams, useSearchParams, useRouter } from 'next/navigation';
+import { useParams} from 'next/navigation';
 import CourseDetails from '../../components/CourseDetails';
 
 export default function CourseDetailsPage() {
   const params = useParams();
-  const searchParams = useSearchParams();
-  const router = useRouter();
+
 
   const urlCourseName = params['course-details'];
 
@@ -18,18 +17,6 @@ export default function CourseDetailsPage() {
   }
 
   const courseName = unformatCourseName(urlCourseName);
-
-  // Fetch the list of courses from your API or state management
-  const courses = []; // Replace with your actual course data
-
-  // Check if the course name exists in the list of courses
-  const courseExists = courses.some(course => course.name === courseName);
-
-  if (!courseExists) {
-    // If the course doesn't exist, redirect to a 404 page or back to the client page
-    router.push('/'); // or router.push('/client');
-    return null;
-  }
 
   return (
     <div id="course-page">
