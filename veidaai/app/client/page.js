f893a20f751ebedfc198d7ed71f2c4c71e75a2e7
smssyquia <sancho.syquia@gmail.com>
2024-08-15 22:35:28 -0700

    Update page.js

--- veidaai/app/client/page.js
+++ veidaai/app/client/page.js
@@ -1,10 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import React, { useState, useEffect } from "react";
 import { useAuth } from "@clerk/nextjs";
-import CourseList from "../../components/CourseList";
-import Link from "next/link";
 import CourseList from "../../components/CourseList";
 import Link from "next/link";
 import "./client.css";
