a97888b029b1d5f1248aef8caf9d053f6cedfb28
smssyquia <sancho.syquia@gmail.com>
2024-08-18 16:00:19 -0700

    added google analytics

--- veidaai/app/pages/_document.js
+++ veidaai/app/pages/_document.js
@@ -5,6 +5,16 @@ export default function Document() {
     <Html>
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        {/* Google tag (gtag.js) */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+          `}
+        </script>
       </Head>
       <body>
         <Main />
