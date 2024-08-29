59c135e0c38ebf69601f8200723108704c13778c
Repello4 <rbukhsh@ucdavis.edu>
2024-08-29 00:20:57 -0700

    Finished the course page and concept page for functionality flashcards mcqs and notes need to be updated

--- veidaai/components/CourseDetails.js
+++ veidaai/components/CourseDetails.js
@@ -4,16 +4,27 @@ import { useAuth } from "@clerk/nextjs";
 import { formatURL } from '@/app/helpers';
 import { FaRegLightbulb, FaRegStickyNote, FaRegQuestionCircle, FaArrowLeft } from 'react-icons/fa';
 import AddContentModal from './AddContentModal';
+import AddConceptModal from './AddConceptModal';
+
 import './course-details.css'; // Ensure this import is correct
 
 const CourseDetails = ({ courseName }) => {
   const { userId } = useAuth();
   const [courseObj, setCourseObj] = useState({});
   const [showAddContentModal, setShowAddContentModal] = useState(false);
+  const [showAddConceptModal, setShowAddConceptModal] = useState(false);
+  const [courseConcepts, setCourseConcepts] = useState([]);
+  const [conceptName, setConceptName] = useState('');
+
+  useEffect(() => {
+    if(userId){
+      fetchCourseObj();
+    }
+  }, [courseName, userId]);
 
   const fetchCourseObj = async () => {
     try {
-      const response = await fetch(`http://localhost:8080/api/get_courses?clerk_id=${userId}`, {
+      const response = await fetch(`http://localhost:8080/api/get_course_concepts?clerk_id=${userId}&course_name=${courseName}`, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
@@ -21,9 +32,9 @@ const CourseDetails = ({ courseName }) => {
       });
       if (response.ok) {
         const data = await response.json();
-        let courseIndex = data.courses.findIndex(course => courseName.localeCompare(course.course_name) == 0);
-        let courseObj = data.courses[courseIndex];
-        setCourseObj(courseObj);
+        
+        setCourseConcepts(data.concepts);
+        console.log(data.concepts);
       } else {
         console.error('Failed to fetch course. Response error: ', response.ok);
       }
@@ -34,6 +45,9 @@ const CourseDetails = ({ courseName }) => {
 
   const handleAddContent = () => {
     setShowAddContentModal(true);
+  };
+  const handleAddConcept = () => {
+    setShowAddConceptModal(true);
   };
 
   const handleContentAdded = () => {
@@ -52,7 +66,10 @@ const CourseDetails = ({ courseName }) => {
     return `${month}/${day}/${year}`;
   };
 
+
+
   return (
+    
     <div className="course-details-container">
       <Link href={`/client`} title={'back to your courses'} className="course-back-arrow-link"><FaArrowLeft/></Link>
       <h2 className="course-title">{courseName}</h2>
@@ -65,8 +82,36 @@ const CourseDetails = ({ courseName }) => {
       <button id="add-content-btn" onClick={handleAddContent} className="course-add-content-btn">
         Add Content
       </button>
+      <button id="add-concept-btn" onClick={handleAddConcept} className="course-add-content-btn">
+        Add Concept
+      </button>
       
-      <div className="course-content">
+
+      <div> 
+      <h3>Course Showing</h3>
+      <p>{courseName}</p>
+      </div>
+
+
+      <div className="course-concepts-container" style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+        {courseConcepts.length > 0 ? (
+          courseConcepts.map((concept, index) => (
+            <div key={index} className="course-concept">
+
+          <Link href={`/concept-details/${concept.concept_name.replace(/\s+/g, '-').toLowerCase()}?courseName=${courseName}`}>
+            <h3>{concept.concept_name}</h3>
+          </Link>
+
+
+              <p>{concept.concept_description}</p>
+            </div>
+          ))
+        ) : (
+          <p>No concepts available.</p> // Message when no concepts are present
+        )}
+      </div>
+
+      {/* <div className="course-content">
       <Link href={`/flashcards/${formatURL(courseName)}`} className="course-study-container">
           <div>
             <FaRegLightbulb className="course-study-icon" />
@@ -88,13 +133,19 @@ const CourseDetails = ({ courseName }) => {
           </div>
           <p>Test your knowledge with multiple choice questions</p>
         </Link>
-      </div>
+      </div> */}
 
       {showAddContentModal && (
         <AddContentModal
           courseName={courseName}
           onClose={() => setShowAddContentModal(false)}
           onContentAdded={handleContentAdded}
+        />
+      )}
+      {showAddConceptModal && (
+        <AddConceptModal
+          courseName={courseName}
+          onClose={() => setShowAddConceptModal(false)}
           onConceptAdded={handleConceptAdded}
         />
       )}
