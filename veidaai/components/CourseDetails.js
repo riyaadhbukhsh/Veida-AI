185bc7c2e4284fea3c77a28c3108daf68dfc5f78
smssyquia <sancho.syquia@gmail.com>
2024-09-02 23:01:31 -0700

    fixed UI for concept and content modals

--- veidaai/components/CourseDetails.js
+++ veidaai/components/CourseDetails.js
@@ -3,16 +3,13 @@ import Link from 'next/link';
 import { useAuth } from "@clerk/nextjs";
 import { formatURL } from '@/app/helpers';
 import { FaRegLightbulb, FaRegStickyNote, FaRegQuestionCircle, FaArrowLeft } from 'react-icons/fa';
-import AddContentModal from './AddContentModal';
 import AddConceptModal from './AddConceptModal';
 
-import './concept-cards.css'; 
-import './course-details.css';
+import "./course-details.css"
 
 const CourseDetails = ({ courseName }) => {
   const { userId } = useAuth();
   const [courseObj, setCourseObj] = useState({});
-  const [showAddContentModal, setShowAddContentModal] = useState(false);
   const [showAddConceptModal, setShowAddConceptModal] = useState(false);
   const [courseConcepts, setCourseConcepts] = useState([]);
   const [conceptName, setConceptName] = useState('');
@@ -44,15 +41,8 @@ const CourseDetails = ({ courseName }) => {
     }
   };
 
-  const handleAddContent = () => {
-    setShowAddContentModal(true);
-  };
   const handleAddConcept = () => {
     setShowAddConceptModal(true);
-  };
-
-  const handleContentAdded = () => {
-    fetchCourseObj(); // Refresh the course data after adding content
   };
 
   const handleConceptAdded = () => {
@@ -73,27 +63,37 @@ const CourseDetails = ({ courseName }) => {
     
     <div className="course-details-container">
       <Link href={`/client`} title={'back to your courses'} className="course-back-arrow-link"><FaArrowLeft/></Link>
-      <h2 className="course-title">{courseName}</h2>
+      <h1>{courseName}</h1>
       {courseObj.exam_date && (
         <p className="course-exam-date">Exam Date: {formatDate(courseObj.exam_date)}</p>
       )}
       {courseObj.description && (
         <p className="course-description">{courseObj.description}</p>
-      )}
-      <button id="add-concept-btn" onClick={handleAddConcept} className="course-add-content-btn">
-        Add Concept
-      </button>
-      
+      )}      
 
-      <div className="concept-cards" style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+      <div className="course-cards">
+        <button className="course-card new-course" onClick={handleAddConcept}>
+          <h2>Add Concept</h2>
+        </button>
+
         {courseConcepts.length > 0 ? (
           courseConcepts.map((concept, index) => (
-            <div key={index} className="concept-card">
-
-          <Link href={`/concept-details/${concept.concept_name.replace(/\s+/g, '-')}?courseName=${courseName}`} className="concept-link">
-            <h2 style = {{textAlign: 'center'}}>{concept.concept_name}</h2>
-          </Link>
-            
+            <div key={index} className="course-card">
+              <Link href={`/concept-details/${concept.concept_name.replace(/\s+/g, '-')}?courseName=${courseName}`} className="course-link">
+                <h2>{concept.concept_name}</h2>
+              </Link>
+              <button className="edit-course-button" onClick={() => handleEditCourse(course)}>
+                <i className="fas fa-pencil-alt"></i>
+              </button>
+              <button
+                className="delete-course-button"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleDeleteCourse(course.course_name);
+                }}
+              >
+                <i className="fas fa-trash-alt"></i>
+              </button>
             </div>
           ))
         ) : (
@@ -101,14 +101,6 @@ const CourseDetails = ({ courseName }) => {
         )}
       </div>
 
-
-      {showAddContentModal && (
-        <AddContentModal
-          courseName={courseName}
-          onClose={() => setShowAddContentModal(false)}
-          onContentAdded={handleContentAdded}
-        />
-      )}
       {showAddConceptModal && (
         <AddConceptModal
           courseName={courseName}
